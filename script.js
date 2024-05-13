e273848ab709ccbbd392cef1c4ce36edcfd4b0bd
Gellokd <adamzaiter23@gmail.com>
2024-05-13 13:25:55 -0500

    gi

---
+++ script.js
@@ -0,0 +1,193 @@
+/*
+Exercise 1: maxOfTwoNumbers()
+
+In this exercise, create a function named maxOfTwoNumbers. It should take two numbers as inputs and return the larger number. If they're equal, return either one.
+
+Exercise 1 has been completed for you:
+*/
+
+const maxOfTwoNumbers = (x, y) => {
+    if (x >= y) {
+      return x;
+    } else {
+      return y;
+    }
+  }
+  
+  console.log('Exercise 1 Result:', maxOfTwoNumbers(3, 9));
+  /*
+Exercise 2: isAdult()
+
+Write a function named isAdult. It should take an age (number) and return 'Adult' if the age is 18 or over and 'Minor' otherwise.
+
+Example: isAdult(21) should return 'Adult'.
+
+Complete the exercise in the space below:
+*/
+
+function isAdult(age) {
+    if (age >= 18) {
+        return 'Adult';
+    } else {
+        return 'Minor';
+    }
+}
+
+console.log('Exercise 2 Result:', isAdult(21));
+
+/*
+Exercise 3: isCharAVowel()
+
+Write a function named isCharAVowel that takes a single character as an argument. It should return true if the character is a vowel and false otherwise. For the purposes of this exercise, the character y should not be considered a vowel.
+
+Example: isCharAVowel('a') should return true.
+
+Complete the exercise in the space below:
+*/
+
+function isCharAVowel(char) {
+    var lowerChar = char.toLowerCase();
+
+    return lowerChar === 'a' || lowerChar === 'e' || lowerChar === 'i' || lowerChar === 'o' || lowerChar === 'u';
+}
+
+
+console.log('Exercise 3 Result:', isCharAVowel("n"));
+
+/*
+Exercise 4: generateEmail()
+
+Create a function named generateEmail. It should take two strings: a name and a domain. It should return a simple email address.
+
+Example: generateEmail('johnsmith', 'example.com') should return 'johnsmith@example.com'.
+
+Complete the exercise in the space below:
+*/
+
+function generateEmail(name, domain) {
+return name + '@' + domain;
+}
+
+console.log('Exercise 4 Result:', generateEmail("adamzaiter23", "gmail.com"));
+
+/*
+Exercise 5: greetUser()
+
+Define a function called greetUser. It should take a name and a time of day (morning, afternoon, evening) and return a personalized greeting.
+
+Example: greetUser('Sam', 'morning') should return "Good morning, Sam!"
+
+Complete the exercise in the space below:
+*/
+
+function greetUser(name, timeOfDay) {
+    var greetings = {
+        'morning': 'Good Moring',
+        'afternoon': 'Good Afternoon',
+        'evening': 'Good evening'
+    };
+    var greeting = greetings[timeOfDay]
+    return `${greeting}, ${name}!`;
+
+}
+
+
+console.log('Exercise 5 Result:', greetUser("Adam", "evening"));
+
+/*
+Exercise 6: reverseString()
+
+Define a function called reverseString. It should take a string and return it with its characters in reverse order. 
+
+Example: reverseString('rockstar') should return the string "ratskcor".
+
+Complete the exercise in the space below:
+*/
+
+function reverseString(string) {
+    return string.split('').reverse().join('');
+
+}
+
+
+console.log('Exercise 6 Result:', reverseString("GeneralAssembly"));
+
+/*
+Exercise 7: checkPalindrome()
+
+Define a function called checkPalindrome. It should take a string and return true if the string is a palindrome (reads the same forwards and backwards) and false otherwise.
+
+Example: checkPalindrome('radar') should return true.
+Example: checkPalindrome('taco') should return false.
+
+Complete the exercise in the space below:
+*/
+
+function checkPalindrome(str) {
+    var lowerStr = str.toLowerCase();
+    return lowerStr === reverseString(lowerStr);
+}
+
+function reverseString(string) {
+    return string.split('').reverse().join('');
+
+}
+console.log('Exercise 7 Result:', checkPalindrome("cube"));
+
+/*
+Exercise 8: maxOfThree()
+
+Define a function, maxOfThree. It should accept three numbers and return the largest among them.
+
+Example: maxOfThree(17, 4, 9) should return 17.
+
+Complete the exercise in the space below:
+*/
+
+function maxOfThree(num1, num2, num3) {
+    return Math.max(num1, num2, num3);
+}
+
+console.log('Exercise 8 Result:', maxOfThree(500, 110, 800));
+
+/*
+Exercise 9: calculateTip()
+
+Create a function called calculateTip. It should take two arguments: the bill amount and the tip percentage (as a whole number). The function should return the amount of the tip.
+
+Example: calculateTip(50, 20) should return 10.
+
+Complete the exercise in the space below:
+*/
+
+function calculateTip(billAmount, tipPercentage) {
+    var tipDecimal = tipPercentage / 100;
+    var tipAmount = billAmount * tipDecimal;
+    return tipAmount
+}
+
+console.log('Exercise 9 Result:', calculateTip(500, 20));
+
+/*
+Exercise 10: convertTemperature()
+
+Write a function named convertTemperature. It takes two arguments: a temperature and a string representing the scale ('C' for Celsius, 'F' for Fahrenheit). Convert the temperature to the other scale.
+
+Example: convertTemperature(32, 'C') should return 89.6 (Fahrenheit).
+Example: convertTemperature(32, 'F') should return 0 (Celsius).
+
+Complete the exercise in the space below:
+*/
+
+function convertTemperature (temperature, scale) {
+    if (scale === 'C') {
+        return (temperature * 9/5) + 32; }
+    else if (scale === 'F') {
+        return (temperature - 32) * 5/9;
+    } else 
+    return "Invalid"
+    }
+
+
+console.log('Exercise 10 Result:', convertTemperature(32, "C"));
+
